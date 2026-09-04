65f48a31061c34d18ae1482fc070fe78c653a754
novakvova <novakvova@gmail.com>
2026-09-04 19:39:43 +0300

    Fix project

--- WebNetflix/WebNetflix/Interfaces/IJwtTokenService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Domain.Entities.Identity;
-
-namespace WebNetflix.Interfaces;
-
-public interface IJwtTokenService
-{
-    Task<string> CreateTokenAsync(UserEntity user);
-}
--- netflix-vite/src/types/user.types.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-    id: number;
-    fullName: string;
-    email: string;
-    image: string;
-}
